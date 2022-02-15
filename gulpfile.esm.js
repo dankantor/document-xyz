b80c0a87ba93f1e6e6d15b3d44606c777a51bc3d
dankantor <dan@Dans-MacBook-Air.local>
2022-02-15 17:39:49 -0500

    using service worker to return from cache and do offline

--- gulpfile.esm.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp';
-import {clean} from './devops/gulp/clean';
-import {rev} from './devops/gulp/rev';
-
-exports.clean = clean;
-exports.rev = rev;
-exports.build = gulp.series(clean, rev);
